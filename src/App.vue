353e979bdb74e746b94cfda340d565d64df0d407
summmermin <110873921+summmermin@users.noreply.github.com>
2023-01-10 14:55:00 +0900

    axios 사용하여 로그인
    axios http프로토콜을 이용한 브라우저에서 서버로 데이터를 주고 받기

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
-  <div>
-<!--    <app-header v-bind:프롭스속성이름="상위 컴포넌트의 데이터이름"></app-header>-->
-    <app-header v-bind:propsdata="str"></app-header>
-  </div>
+  <form v-on:submit.prevent="submitForm">
+    <!--.prevent -->
+    <!--event.preventDefault() 위에는 이거랑 같은 역할-->
+    <div>
+      <label for="username">id :</label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw :</label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
 </template>
 
 <script>
-import AppHeader from "@/components/AppHeader.vue";
+import axios from "axios";
+// axios는 http프로토콜을 이용한 브라우저에서 서버로 데이터를 주고 받기 위한 라이브러리.
 
 export default {
   data: function () {
-    return {str: 'Header'}
+    return {
+      username: '',
+      password: ''
+    }
   },
-  components: {
-    'app-header': AppHeader
+  methods: {
+    submitForm: function () {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+          .then(function (response) {
+            console.log(response)
+          })
+          .catch(function (error) {
+            console.log(error)
+          });
+      //post는 데이터를 생성, 저장하거나 할 때 쓰는 http 방법, methods
+    }
   }
 }
 </script>
